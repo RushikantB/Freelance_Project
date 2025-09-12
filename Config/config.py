bf9e215f98b3bcba60a56668de9af06646f9694c
RushikantB <rushikantbhosale@gmail.com>
2025-09-12 17:41:46 +0530

    WIP: local changes before rebase

--- Config/config.py
+++ Config/config.py
@@ -1,12 +1,12 @@
 class Config:
-    ENV = "UAT" #UAT or QA
-    BASE_URL = "https://uat.pacehr.thesnfist.com/login"
-    USERNAME = "rbhosale@health.saisystems.com"
-    PASSWORD = "Sai@1234"
-    BROWSER = "chrome" # "chrome" or "edge" 
-    UAT_URL = "https://uat.pacehr.thesnfist.com/login"
-    QA_URL = "https://qa.pacehr.thesnfist.com/login"
-    QA_PASSWORD = "Sai@12345"
-    UAT_PASSWORD = "Sai@1234"
-    SUPER_ADMIN = "schitare@saisystems.com"
-    SUPER_ADMIN_PASSWORD = "schitare"
+    ENV = "UAT"  # UAT or QA
+    BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    USERNAME = "Admin"
+    PASSWORD = "admin123"
+    BROWSER = "chrome"  # "chrome" or "edge"
+    UAT_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    QA_URL = ""
+    QA_PASSWORD = ""
+    UAT_PASSWORD = "admin123"
+    SUPER_ADMIN = ""
+    SUPER_ADMIN_PASSWORD = ""
